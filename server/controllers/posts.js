c770ba034157d7f5878f9d04cc6493e94aa46c0d
Skyes Crawford <akashiseijurou414@gmail.com>
2021-02-28 07:17:54 +0700

    create basic functionality

---
+++ server/controllers/posts.js
@@ -0,0 +1,22 @@
+import Post from "../models/post.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+export const createPost = async (req, res) => {
+  const { title, message, selectedFile, creator, tags } = req.body;
+
+  const newPost = new Post({ title, message, selectedFile, creator, tags });
+
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/posts", postRoutes);
+
+mongoose.connect("mongodb://localhost:27017/memoriesApp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+app.get("/", (req, res) => {
+  res.send("connected!");
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port: ${PORT}`);
+});
