b7e1f63e18c9d7cdbc80b9c342401fb92433d4c1
Skyes Crawford <akashiseijurou414@gmail.com>
2021-03-03 16:07:59 +0700

    finishing

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -3,7 +3,7 @@ import Post from "../models/post.js";
 
 export const getPosts = async (req, res) => {
   try {
-    const posts = await Post.find({});
+    const posts = await Post.find();
     res.status(200).json(posts);
   } catch (error) {
     res.status(404).json({ message: error.message });
--- server/index.js
+++ server/index.js
@@ -12,8 +12,7 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL =
-  process.env.CONNECTION_URL || "mongodb://localhost:27017/memoriesApp";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 app.use("/posts", postRoutes);
